ce71f69793c98040f349118e6648d8d4ccd95d1a
JianDong-Z <16607979586@163.com>
2025-05-15 17:31:31 +0800

    save1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- html -->
+  <div class="app">
+    <h1>你好啊！</h1>
+    <Person/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Person from './components/Person.vue'
+  export default {
+  name:'App',
+  components:{Person} //注册组件
+  }
+</script>
+
+<style>
+  .app{
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+    }
+</style>
